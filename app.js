a2cff16f51d6885884b49de18109dcd471a1f08f
H. alrahmani <h.m.alrahmani@gmail.com>
2022-04-20 00:18:19 +0300

    FINISHED task email script - API

--- app.js
+++ app.js
@@ -27,16 +27,16 @@ for (let i = 0; i < result.Sheet1.length; i++) {
     to: "h.m.alrahmani@outlook.com",
     from: "h.m.alrahmani@gmail.com",
     subject: "1, 2, test, test",
-    html: `<div class="container" style= "background-img: url("border-design.jpg"); width:100%; display: flex; justify-content: center; align-items: center;">
-                  <h1>Certificate of Completion</h1>
-                  <p>This certificate is awarded to</p>
-                  <h2 style="margin:40px;">${name}</h2>
-                  <p>for the secssful completion of</p>
-                  <h4 style="margin:20px;">${course}</h4>
-                  <h6>with a grade of ${grade} on ${day}/${month}/${year}</h6>
-                  <h5>Someone Important</h5>
-                  <h6>CEO of the world</h6>
-               </div>`,
+    html: `<div class="container" style= "width: 100%; height: 100; padding: 600px; display: inline; justify-content: center; text-align: center;">
+                <h1 style="border-top-style: double; padding-top: 180px; font-size: xx-large;"><strong> Certificate of Completion</strong></h1>
+                <p><strong> certificate is awarded to </strong></p>
+                <h2 style="margin:40px;">${name}</h2>
+                <h4>for the secssful completion of</h4>
+                <h3 style="margin:20px;">${course}</h3>
+                <h4 style="margin-bottom: 80px;">with a grade of ${grade} on ${day}/${month}/${year}</h4>
+                <h5>Someone Important</h5>
+                <h6 style="border-bottom-style: double; padding-bottom: 100px;">CEO of the world</h6>
+            </div>`,
   };
 
   sgMail
